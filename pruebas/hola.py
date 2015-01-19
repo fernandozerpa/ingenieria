25b5e9793a8364dd6cf8f26782f7afa4a643e294
fernandozerpa <zerpa.fernando48@gmail.com>
2015-01-19 17:39:52 -0430

    mi primer commit

---
+++ pruebas/hola.py
@@ -0,0 +1,16 @@
+'''
+Created on Jan 17, 2015
+
+@author: JuNiOr
+'''
+import unittest
+
+def factorial(n):
+    return 1 if n < 1 else n * factorial(n-1)
+
+class tester (unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(1, factorial(1))
+
+if __name__ == "__main__":
+    unittest.main()
